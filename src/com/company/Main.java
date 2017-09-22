dcedd18d868e448cee3382074193f4e8cc3fbe8f
KorJuk69 <korjuk1@gmail.com>
2017-09-22 17:40:58 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,26 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
+        int n = scanner.nextInt();
+        int a [];
+        int schet=0;
+        int k = 0;
 
+        for (int m=0;m<10000;m++){
+
+            for(int i=k;i<n;i++){
+
+                a = new int[n];
+                a[i] = scanner.nextInt();
+                k++;
+                if (a[i] == m){
+                    schet++;
+                    break;
+                }
+            }
+            if(m==n)break;
+        }
+
+        System.out.print(schet);
     }
 }
